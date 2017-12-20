93a28cb58b534e91facafa615fadb7c9f1596b6f
rtv22 <rtvcojigor@gmail.com>
2017-12-20 13:17:46 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,38 @@
 #include <vector>
 #include <algorithm>
 
-SCENARIO("algorithm", "[algorithm]"){
+SCENARIO("init", "[init]"){
 	Graph test_;
 	REQUIRE(test_.count() == 0);
+}
+
+SCENARIO("algorithm", "[algorithm]"){
+	Graph test(6);
+	test.AddWeightedEdge(0, 1, 6);
+	test.AddWeightedEdge(0, 2, 1);
+	test.AddWeightedEdge(0, 3, 5);
+	test.AddWeightedEdge(1, 0, 6);
+	test.AddWeightedEdge(1, 2, 5);
+	test.AddWeightedEdge(1, 4, 3);
+	test.AddWeightedEdge(2, 0, 1);
+	test.AddWeightedEdge(2, 1, 5);
+	test.AddWeightedEdge(2, 3, 5);
+	test.AddWeightedEdge(2, 4, 6);
+	test.AddWeightedEdge(2, 5, 4);
+	test.AddWeightedEdge(3, 0, 5);
+	test.AddWeightedEdge(3, 2, 5);
+	test.AddWeightedEdge(3, 5, 2);
+	test.AddWeightedEdge(4, 1, 3);
+	test.AddWeightedEdge(4, 2, 6);
+	test.AddWeightedEdge(4, 5, 6);
+	test.AddWeightedEdge(5, 3, 2);
+	test.AddWeightedEdge(5, 2, 4);
+	test.AddWeightedEdge(5, 4, 6);
+	vector<pair<int, edge>> k; 
+	k.push_back(make_pair(1, edge(0, 2)));
+	k.push_back(make_pair(2, edge(3, 5)));
+	k.push_back(make_pair(3, edge(1, 4)));
+	k.push_back(make_pair(4, edge(2, 5)));
+	k.push_back(make_pair(5, edge(1, 2)));
+	REQUIRE((k == test.kraskal()) == true);
 }
